5f86686af1ad9dd82b6f611413c1077ae520aeee
BlackKoder <81838709+BlackKoder@users.noreply.github.com>
2025-11-11 09:48:24 -0400

    Update listado_reportes.js

--- listado_reportes.js
+++ listado_reportes.js
@@ -20,5 +20,103 @@ const LISTA_REPORTES = [
         month: 07, year: 2024,
         path: "./reportes/Balance del mes de julio 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
     },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Agosto 2024", 
+        month: 08, year: 2024,
+        path: "./reportes/Balance del mes de agosto 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Septiembre 2024", 
+        month: 09, year: 2024,
+        path: "./reportes/Balance del mes de septiembre 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Octubre 2024", 
+        month: 10, year: 2024,
+        path: "./reportes/Balance del mes de octubre 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Noviembre 2024", 
+        month: 11, year: 2024,
+        path: "./reportes/Balance del mes de noviembre 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Diciembre 2024", 
+        month: 12, year: 2024,
+        path: "./reportes/Balance del mes de diciembre 2024 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Enero 2025", 
+        month: 01, year: 2025,
+        path: "./reportes/Balance del mes de enero 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Febrero 2025", 
+        month: 02, year: 2025,
+        path: "./reportes/Balance del mes de febrero 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Marzo 2025", 
+        month: 03, year: 2025,
+        path: "./reportes/Balance del mes de marzo 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Abril 2025", 
+        month: 04, year: 2025,
+        path: "./reportes/Balance del mes de abril 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Mayo 2025", 
+        month: 05, year: 2025,
+        path: "./reportes/Balance del mes de mayo 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Junio 2025", 
+        month: 06, year: 2025,
+        path: "./reportes/Balance del mes de junio 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Julio 2025", 
+        month: 07, year: 2025,
+        path: "./reportes/Balance del mes de julio 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Agosto 2025", 
+        month: 08, year: 2025,
+        path: "./reportes/Balance del mes de agosto 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
+
+    { 
+        type: "Estancia", 
+        name: "Estancia Canina Septiembre 2025", 
+        month: 09, year: 2025,
+        path: "./reportes/Balance del mes de septiembre 2025 - DOJOPETS - Hojas de cálculo de Google.pdf" 
+    },
     // Añade más reportes aquí...
 ];
