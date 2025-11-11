fc3da601e1b3b4f5d5ddf5b14ceda3b9e2b67a25
BlackKoder <81838709+BlackKoder@users.noreply.github.com>
2025-11-11 09:32:30 -0400

    Update listado_reportes.js

--- listado_reportes.js
+++ listado_reportes.js
@@ -11,14 +11,14 @@ const LISTA_REPORTES = [
         type: "Estancia", 
         name: "Estancia Canina Mayo 2024", 
         month: 05, year: 2024,
-        path: "./reportes/Balance del mes de mayo 2024 - DOJOPETS.pdf" 
+        path: "./reportes/Balance del mes de mayo 2024-DOJOPETS-Hojas de cálculo de Google.pdf" 
     },
 
     { 
         type: "Estancia", 
         name: "Estancia Canina Julio 2024", 
         month: 07, year: 2024,
-        path: "./reportes/Balance del mes de julio 2024 - DOJOPETS.pdf" 
+        path: "./reportes/Balance del mes de julio 2024-DOJOPETS-Hojas de cálculo de Google.pdf" 
     },
     // Añade más reportes aquí...
 ];
